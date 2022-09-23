5a84004419532a6ebbf826e1ce11b6bec33888f1
dlopez-s <dlopez-s@student.42.com>
2022-09-23 16:52:06 +0200

    .

--- ft_substr.c
+++ ft_substr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lopezz <lopezz@student.42.fr>              +#+  +:+       +#+        */
+/*   By: dlopez-s <dlopez-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 00:34:49 by lopezz            #+#    #+#             */
-/*   Updated: 2022/09/23 12:23:35 by lopezz           ###   ########.fr       */
+/*   Updated: 2022/09/23 16:25:41 by dlopez-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,22 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*ptr;
 	size_t	cont;
 
+	if (s == 0)
+		return (NULL);
+	if (start > (unsigned int)ft_strlen(s))
+		return (ft_strdup(""));
+	cont = ft_strlen(start + s);
+	if (cont < len)
+		len = cont;
 	ptr = (char *)malloc((len + 1) * sizeof(char));
-	cont = 0;
 	if (ptr == 0)
 		return (NULL);
-	if (start > ft_strlen((char *)s))
-		return (ft_strdup(""));
-	while (cont < len && s[start] != '\0')
-	{
-		ptr[cont] = s[start];
-		start++;
-		cont++;
-	}
-	ptr[cont] = '\0';
+	ft_strlcpy(ptr, s + start, len + 1);
 	return (ptr);
 }
 
 /*
 #include <stdio.h>
-
 int main()
 {
 	char str[] = "lorem ipsum dolor sit amet";
--- libft.h
+++ libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lopezz <lopezz@student.42.fr>              +#+  +:+       +#+        */
+/*   By: dlopez-s <dlopez-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/13 12:07:30 by dlopez-s          #+#    #+#             */
-/*   Updated: 2022/09/23 12:51:58 by lopezz           ###   ########.fr       */
+/*   Updated: 2022/09/23 15:51:57 by dlopez-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,6 +42,7 @@ char	*ft_strdup(const char *s1);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
