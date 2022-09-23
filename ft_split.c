bdd48ee338e5171619567e8be275d9cfcf92d8ec
dlopez-s <dlopez-s@student.42.com>
2022-09-23 20:06:53 +0200

    .

--- ft_split.c
+++ ft_split.c
@@ -6,13 +6,42 @@
 /*   By: dlopez-s <dlopez-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/23 15:51:17 by dlopez-s          #+#    #+#             */
-/*   Updated: 2022/09/23 17:26:54 by dlopez-s         ###   ########.fr       */
+/*   Updated: 2022/09/23 19:38:10 by dlopez-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	**ft_split(char const *s, char c)
+//1. hacer contador de palabras
+//2. funcion para escribir cada palabra nueva (con malloc)
+//3. split
+
+static int ft_contw(char *str, char c)
+{
+	int	i;
+	int n_words;
+
+	i = 0;
+	n_words = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == c)
+			n_words++;
+		i++;
+	}
+	n_words++;
+	return (n_words);
+}
+
+/* char	**ft_split(char const *s, char c)
 {
 	
+} */
+
+#include <stdio.h>
+
+int main()
+{
+	char str[] = "viva er beti";
+	printf("%d", ft_contw(str, 'a'));
 }
