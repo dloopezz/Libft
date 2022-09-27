0c9d20a690992e538b7c8efedcbfc248e3c62cfa
dlopez-s <dlopez-s@student.42.com>
2022-09-27 16:39:15 +0200

    .

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -3,15 +3,91 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lopezz <lopezz@student.42.fr>              +#+  +:+       +#+        */
+/*   By: dlopez-s <dlopez-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 21:59:01 by lopezz            #+#    #+#             */
-/*   Updated: 2022/09/23 12:35:09 by lopezz           ###   ########.fr       */
+/*   Updated: 2022/09/27 16:18:45 by dlopez-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int	ft_inicio(const char *s1, const char *set)
+{
+	size_t	i;
+	size_t	len;
+	int		flag;
+
+	len = ft_strlen(s1);
+	i = 0;
+	flag = -1;
+	while (i < len && flag < 0)
+	{
+		if (ft_strchr(set, s1[i]) == 0)
+		{
+			flag = 0;
+			i--;
+		}
+		i++;
+	}
+	return (i);
+}
+
+static int	ft_final(const char *s1, const char *set)
+{
+	size_t	i;
+	size_t	len;
+	int		flag;
+
+	len = ft_strlen(s1);
+	i = 0;
+	flag = -1;
+	while (i < len && flag < 0)
+	{
+		if (ft_strchr(set, s1[len - 1 - i]) == 0)
+		{
+			flag = 0;
+			i--;
+		}
+		i++;
+	}
+	return (len - i);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	int		inicio;
+	int		final;
+	char	*str;
+
+	if (!s1)
+		return (NULL);
+	if (!set)
+		return (ft_strdup(s1));
+	inicio = ft_inicio(s1, set);
+	final = ft_final(s1, set);
+	if (inicio >= final)
+		return (ft_strdup(""));
+	str = (char *)malloc((final - inicio + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	ft_strlcpy(str, s1 + inicio, final - inicio + 1);
+	return (str);
+}
+
+/* 
+#include <stdio.h>
+
+int main()
+{
+	char s1[] = "   Hola que tal   ";
+	char set[] = "que tal";
+	
+	printf("%s", ft_strtrim(s1, set));
+	return (0);
+}
+
+FUNCIÓN ORIGINAL CON BREAK:
 static int	ft_inicio(const char *s1, const char *set)
 {
 	size_t	i;
@@ -65,7 +141,6 @@ char	*ft_strtrim(char const *s1, char const *set)
 	return (str);
 }
 
-/*
 #include <stdio.h>
 
 int main()
